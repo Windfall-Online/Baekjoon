b7796a3d7aebaf0584baf34f83026727adcbbaf3
Windfall-Online <109807780+Windfall-Online@users.noreply.github.com>
2022-07-26 01:26:06 -0700

    string

---
+++ 10988/10988.cpp
@@ -0,0 +1,18 @@
+﻿#include "bits/stdc++.h"
+using namespace std;
+
+string str;
+string temp;
+
+int main()
+{
+	cin >> str;
+	temp = str;
+	reverse(temp.begin(), temp.end());
+	
+	if (str == temp)
+		cout << "1";
+	else
+		cout << "0";
+}
+
