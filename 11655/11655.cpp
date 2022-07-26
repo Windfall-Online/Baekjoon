934cb99f45dcb83bc401bcddf25fdf7001e71dd2
Windfall-Online <109807780+Windfall-Online@users.noreply.github.com>
2022-07-26 01:26:24 -0700

    string

---
+++ 11655/11655.cpp
@@ -0,0 +1,31 @@
+﻿#include <iostream>
+#include <string>
+using namespace std;
+
+string str;
+
+int main()
+{
+  
+	getline(cin, str);
+	for (string::size_type i = 0; i < str.size(); ++i)
+	{
+		if (str[i] >= 'A' && str[i] <= 'Z')
+		{
+			if (str[i] + 13 > 'Z')
+				str[i] = str[i] - 26 + 13;
+			else
+				str[i] = str[i] + 13;
+		}
+		else if (str[i] >= 'a' && str[i] <= 'z')
+		{
+			if (str[i] + 13 > 'z')
+				str[i] = str[i] + 13 - 26;
+			else
+				str[i] = str[i] + 13;
+		}
+	}
+	cout << str;
+   
+}
+
