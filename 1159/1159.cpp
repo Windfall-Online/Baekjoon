b6f1678786871d2a93b2df70e6d064ae08c17e1d
Windfall-Online <109807780+Windfall-Online@users.noreply.github.com>
2022-07-26 01:26:16 -0700

    string

---
+++ 1159/1159.cpp
@@ -0,0 +1,36 @@
+﻿#include "bits/stdc++.h"
+using namespace std;
+
+
+
+int alpha[26];
+string str;
+string ret;
+int cnt;
+
+int main()
+{
+	cin >> cnt;
+	for (int i = 0; i < cnt; ++i)
+	{
+		cin >> str;
+		alpha[str[0] - 'a']++;
+	}
+
+	for (int i = 0; i < 26; ++i)
+	{
+		if(alpha[i] >= 5)
+			ret += i + 'a';
+
+	}
+	// ret문자열이 존재하면
+	if (ret.size())
+	{
+		cout << ret;
+	}
+	else
+	{
+		cout << "PREDAJA";
+	}
+}
+
