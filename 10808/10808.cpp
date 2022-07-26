0fe62a8b4ab1304d3bb5161f868a17d6a85f6a63
Windfall-Online <109807780+Windfall-Online@users.noreply.github.com>
2022-07-26 01:25:58 -0700

    string

---
+++ 10808/10808.cpp
@@ -0,0 +1,22 @@
+﻿#include "bits/stdc++.h"
+using namespace std;
+
+string str;
+const int BUF_SIZE = 26;
+int cnt[BUF_SIZE];
+
+
+int main()
+{
+
+    cin >> str;
+
+    for (char a : str)
+    {
+        cnt[a - 'a']++;
+    }
+    for (int i = 0; i < 26; ++i)
+        cout << cnt[i] << " ";
+    
+}
+
