25f7e06e87e0ebfa7e716d9a948a9370136fa7b0
Windfall-Online <109807780+Windfall-Online@users.noreply.github.com>
2022-07-26 01:28:37 -0700

    prefix sum

---
+++ 2559/2559.cpp
@@ -0,0 +1,40 @@
+﻿#include <iostream>
+#include "bits/stdc++.h"
+using namespace std;
+
+// 맨 마지막은 더하지 않는것이 핵심?
+// 시간 초과?
+
+int preSum[100001];
+int n;
+int k;
+int temp;
+int ret = -10000004;
+
+int main()
+{
+
+	cin >> n >> k;
+
+	for (int i = 1; i <= n; ++i)
+	{
+		cin >> temp;
+		preSum[i] = preSum[i - 1] + temp;
+	}
+	for (int i = 1; i <= n; ++i)
+	{
+		cout << preSum[i] << " ";
+	}
+	cout << endl;
+	for (int i = k; i <= n; ++i)
+	{	
+		cout << preSum[i] << " " << preSum[i - k] << " " << preSum[i] - preSum[i - k] << "      ";
+		ret = max(ret, preSum[i] - preSum[i - k]);
+	}
+	
+
+
+
+	
+}
+
