523f22f58e0b591a71d2b88e9551f81131d08a7b
Windfall-Online <109807780+Windfall-Online@users.noreply.github.com>
2022-07-26 01:28:17 -0700

    sort

---
+++ 2309/2309.cpp
@@ -0,0 +1,30 @@
+﻿#include <iostream>
+#include "bits/stdc++.h"
+using namespace std;
+
+
+const int BUF_SIZE = 9;
+
+int dwarfHeights[BUF_SIZE];
+
+int main()
+{
+    for (int i = 0; i < 9; ++i)
+        cin >> dwarfHeights[i];
+
+    sort(dwarfHeights, dwarfHeights + BUF_SIZE);
+
+    while (next_permutation(dwarfHeights, dwarfHeights + BUF_SIZE))
+    {
+        int sum = 0;
+        for (int i = 0; i < 7; ++i)
+            sum += dwarfHeights[i];
+       
+        if (sum == 100)
+            break;
+    }
+    for (int i = 0; i < 7; ++i)
+        cout << dwarfHeights[i] << endl;
+    
+}
+
