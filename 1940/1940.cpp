97d9dbadce9641ae733e5634fc0e34f7afb5c725
Windfall-Online <109807780+Windfall-Online@users.noreply.github.com>
2022-07-26 01:28:05 -0700

    two pointer

---
+++ 1940/1940.cpp
@@ -0,0 +1,42 @@
+﻿#include "bits/stdc++.h"
+using namespace std;
+
+
+int numArr[15001];
+int first;
+int last;
+int N;
+int M;
+int tempNum;
+int cnt;
+int main()
+{
+	ios_base::sync_with_stdio(false);
+	cin.tie(nullptr);
+	cout.tie(nullptr);
+
+	cin >> N >> M;
+	last = N - 1;
+	for (int i = 0; i < N; ++i)
+	{
+		cin >> tempNum;
+		numArr[i] = tempNum;
+	}
+	sort(numArr, numArr + N);
+	while (first < last)
+	{
+		if ((numArr[first] + numArr[last]) < M)
+			first++;
+		else if ((numArr[first] + numArr[last]) > M)
+			last--;
+		else if ((numArr[first] + numArr[last]) == M)
+		{
+			cnt++;
+			first++;
+			last--;
+		}
+	}
+	cout << cnt;
+
+}
+
