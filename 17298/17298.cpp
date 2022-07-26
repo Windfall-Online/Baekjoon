315f9dcbba9cbc5833cad6f0be6c4e5c998f6ed0
Windfall-Online <109807780+Windfall-Online@users.noreply.github.com>
2022-07-26 01:27:23 -0700

    stack

---
+++ 17298/17298.cpp
@@ -0,0 +1,36 @@
+﻿#include <iostream>
+#include <vector>
+#include <stack>
+using namespace std;
+
+
+int main()
+{	
+	int n;
+	cin >> n;
+	vector<int> a(n);
+	vector<int> ans(n);
+	for (int i = 0; i < n; ++i)
+	{
+		cin >> a[i];
+	}
+	stack<int> s;
+	for (int i = 0; i < n; ++i)
+	{
+		while (!s.empty() && a[s.top()] < a[i])
+		{
+			ans[s.top()] = a[i];
+			s.pop();
+		}
+		s.push(i);
+	}
+	while (!s.empty())
+	{
+		ans[s.top()] = - 1;
+		s.pop();
+	}
+	for (int i = 0; i < n; ++i)
+	{
+		cout << ans[i] << ' ';
+	}
+}
